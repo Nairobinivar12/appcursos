825ba4191d3ce97888e14ae84d00dbf4d5f0e902
Nairobinivar12 <ingenieranairobi@gmail.com>
2021-10-07 12:14:52 +0200

    subiendo el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactameComponent } from './components/contactame/contactame.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { EstudiantesComponent } from './components/estudiantes/estudiantes.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ProfesoresComponent } from './components/profesores/profesores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactameComponent,
+    CursosComponent,
+    EstudiantesComponent,
+    FooterComponent,
+    NavbarComponent,
+    NosotrosComponent,
+    ProfesoresComponent
   ],
   imports: [
     BrowserModule,
